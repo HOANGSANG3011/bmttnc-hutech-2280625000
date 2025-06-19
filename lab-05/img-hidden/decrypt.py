b42b66adf7b4f0524d41213e7069a46827291c4c
hoangsang <trangnotebook@gmail.com>
2025-06-19 15:14:50 +0700

    [add] lab-05 img-hidden.

--- lab-05/img-hidden/decrypt.py
+++ lab-05/img-hidden/decrypt.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 # Hàm giải mã thông điệp từ ảnh
 def decode_image(encoded_image_path):
-    img = Image.open(encoded_image_path)  # Mở ảnh đã mã hóa
+    img = Image.open(encoded_image_path)  # Mở ảnh đã mã hóa   
     width, height = img.size  # Lấy kích thước của ảnh
     binary_message = ""  # Chuỗi nhị phân để lưu thông điệp
 
@@ -12,7 +12,7 @@ def decode_image(encoded_image_path):
         for col in range(width):
             pixel = img.getpixel((col, row))  # Lấy màu pixel tại vị trí (col, row)
             for color_channel in range(3):  # Duyệt qua các kênh màu RGB
-                binary_message += format(pixel[color_channel], '08b')[-1]  # Lấy bit cuối của mỗi kênh màu
+                binary_message += format(pixel[color_channel], '08b')[-1]  # Lấy bit cuối của mỗi kênh màu  
 
     # Chuyển nhị phân thành thông điệp văn bản
     message = ""
